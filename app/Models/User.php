8e43da57e7ce8993877d0a548072809afbca3fdb
rezi_sirbiladze <rezi.sirbiladze2@gmail.com>
2021-09-06 14:33:57 +0200

    añadido mmc de post y comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Posts::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comments::class);
+    }
+
 }
